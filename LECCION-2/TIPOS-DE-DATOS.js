c5728caa660b063da4af79ac6504f8a94a6d4874
Alex420 <edgaring1998@gmail.com>
2021-07-16 12:57:01 -0500

    Mistakes

--- LECCION-2/TIPOS-DE-DATOS.js
+++ LECCION-2/TIPOS-DE-DATOS.js
@@ -6,4 +6,22 @@ en JavaScript.
     // Tipo de dato string 
 var nombre = "Alex";
 console.log(nombre);
+console.log(typeof nombre)
+    //Tipo de dato numerico
+var numero = 1000;
+console.log(numero);   
+console.log(typeof numero)
+    //tipo de dato objeto
+    var objeto = {
+        nombre : "Alex",
+        apellido: "Hernandez",
+        edad :  "23",
+
+    }
+    console.log(objeto);
+/* EN JavaScript las variables son dinamicas y cambian de acuerdo al tipo de dato que almacenes.
+Con la funcion "typeof" sabras que tipo de variable almacena
+*/
+nombre = 10;
+console.log(typeof nombre);
 
