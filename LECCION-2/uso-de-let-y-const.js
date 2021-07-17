6b391746f0b1070fddcb2e7439e6edbf89675392
Alex420 <edgaring1998@gmail.com>
2021-07-17 01:12:45 -0500

    ser-constante

---
+++ LECCION-2/uso-de-let-y-const.js
@@ -0,0 +1,9 @@
+/*para definir una variable que puede cambiar su valor usaremos
+let*/
+let nombre;
+nombre = "Alex";
+console.log(let);
+/*Para definir una variable cuyo valor no cambiarausaremos
+const */
+const apellido = "jimenez";
+//apellido = "juarez"; //Variable  constante y no cambiara su valor.
